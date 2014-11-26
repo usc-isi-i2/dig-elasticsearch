748bf7e5542386b3d9469996865df15a5d72c97e
szeke <szekely1401@gmail.com>
2014-11-25 18:13:03 -0800

    Use settings file

--- types/webpage/scripts/load-many.sh
+++ types/webpage/scripts/load-many.sh
@@ -5,16 +5,19 @@
 curl -XGET "http://karma-dig-service.cloudapp.net:55310/dig/page/_search?pretty=true" -d'
 {   
     "query": {"match_all": {}},
-    "size":  10
+    "size":  3000
 }' > files-to-load.json
 
 # Clear the local elastic search
 #
 curl -XDELETE localhost:9200/dig
 
+curl -XPOST localhost:9200/dig/ -d @../webpage-settings.json
+
 # Reinitialize the mapping
 #
-curl -XPUT localhost:9200/dig -d @../webpage-mapping.json
+#curl -XPUT localhost:9200/dig -d @../webpage-mapping.json
+curl -XPUT localhost:9200/dig/page/_mapping -d @../webpage-mapping-english.json
 
 # Load the objects we retrieved
 #
