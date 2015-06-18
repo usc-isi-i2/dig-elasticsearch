405ca5cc347cb6bd686f927b424ea2fd04660416
Amandeep Singh <amandeep.s.saggu@gmail.com>
2015-06-18 15:25:46 -0700

    handle the case when no records are returned

--- elasticsearch/src/main/java/edu/isi/dig/elasticsearch/Main.java
+++ elasticsearch/src/main/java/edu/isi/dig/elasticsearch/Main.java
@@ -1,6 +1,7 @@
 package edu.isi.dig.elasticsearch;
 
 import edu.isi.dig.elasticsearch.mapreduce.driver.ESProcessor;
+import edu.isi.dig.elasticsearch.mapreduce.inputformat.ESRecordReader;
 
 public class Main {
 
@@ -8,6 +9,9 @@ public class Main {
 		
 		ESProcessor esp = new ESProcessor();
 		esp.run(args);
+		
+		//ESRecordReader esr = new ESRecordReader();
+		//System.out.println(esr.timestampToEpoch("2009-08-10T13:10Z"));
 
 	}
 
