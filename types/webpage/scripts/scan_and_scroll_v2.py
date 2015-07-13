dc9f2ae9ee2556d0fc09d617f8dcb6f64816fb87
Amandeep Singh <amandeep.s.saggu@gmail.com>
2015-07-13 15:16:36 -0400

    write a string, scanandscroll

--- types/webpage/scripts/scan_and_scroll_v2.py
+++ types/webpage/scripts/scan_and_scroll_v2.py
@@ -34,7 +34,7 @@ def scanandscroll(index, doctype, query, hostname="localhost", port=9200, userna
 
             for i in range(len(page['hits']['hits'])):
                 #print page['hits']['hits'][i]
-                f.write(page['hits']['hits'][i])
+                f.write(str(page['hits']['hits'][i]))
             #print "scroll size: " + str(scroll_size)
             # Do something with the obtained page
     f.close()
